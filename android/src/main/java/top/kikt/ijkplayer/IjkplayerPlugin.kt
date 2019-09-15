91da844e67d0f11cb9bbc8af40ecb6084b99fc5a
jiangyong <jiang@hy12371.com>
2019-09-15 22:49:10 +0800

    解决冲突

--- android/src/main/java/top/kikt/ijkplayer/IjkplayerPlugin.kt
+++ android/src/main/java/top/kikt/ijkplayer/IjkplayerPlugin.kt
@@ -8,7 +8,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import tv.danmaku.ijk.media.player.IjkMediaPlayer
+import av.danmaku.ijk.media.player.IjkMediaPlayer
 
 
 /**
